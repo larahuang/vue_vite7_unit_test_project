4e34dc3276ed6b544f0222c25c7e4c3b9a5a78dc
lara huang <lara1105huang@gmail.com>
2025-07-27 19:13:47 +0800

    安裝單元測試與執行啟動單元測試指令

---
+++ src/components/HelloWorld.spec.js
@@ -0,0 +1,25 @@
+//引入描述、它、期望
+import { describe, it, expect } from 'vitest'
+//引入測試實用程式
+import { mount } from '@vue/test-utils'
+//引入元件
+import HelloWorld from './HelloWorld.vue'
+
+describe('HelloWorld', () =>
+{
+    //1+1 應該是2
+  it('1+1 應該是2', () => {
+    expect(1 + 1).toBe(2)
+  })
+    //正確渲染
+  it('正確渲染', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
+    //在此測試檔案中使用 jsdom
+  it('在此測試檔案中使用 jsdom', () => {
+    const element = document.createElement('div')
+    element.innerHTML = '<p>Hello, HTML!</p>'
+    expect(element.innerHTML).toBe('<p>Hello, HTML!</p>')
+  })
+})
